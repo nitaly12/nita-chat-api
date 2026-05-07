a4b144a008097d40bf53f47423852d090c6fb4f2
nitaly12 <nitaly086@gmail.com>
2026-05-07 15:05:56 +0700

    fixed refresh token

--- src/main/java/org/example/chat/repository/RefreshTokenRepository.java
+++ src/main/java/org/example/chat/repository/RefreshTokenRepository.java
@@ -4,6 +4,8 @@ import org.example.chat.entity.RefreshToken;
 import org.example.chat.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
@@ -11,6 +13,7 @@ import java.util.Optional;
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByToken(String token);
 
-    @Modifying
-    int deleteByUser(User user);
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query("delete from RefreshToken rt where rt.user = :user")
+    int deleteByUser(@Param("user") User user);
 }
